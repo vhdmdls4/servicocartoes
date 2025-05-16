c5b9c0dbda550db4ec4bdf2dbcd8c8ee906efd83
Victor <vhduartelima@hotmail.com>
2025-05-15 22:32:42 -0300

    fix: update CreditCardController request mapping to use hyphen instead of underscore

--- src/main/java/com/victorduarte/servicocartoes/application/entrypoint/CreditCardController.java
+++ src/main/java/com/victorduarte/servicocartoes/application/entrypoint/CreditCardController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("api/credit_card")
+@RequestMapping("api/credit-card")
 @RestController
 @RequiredArgsConstructor
 public class CreditCardController {
