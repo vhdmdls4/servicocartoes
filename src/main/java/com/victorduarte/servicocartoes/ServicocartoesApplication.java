8d589bc7c603a37c915f3a70f2fd8846387d2515
Victor <vhduartelima@hotmail.com>
2025-05-30 23:46:58 -0300

    add RabbitMQ configuration and enable Rabbit support in the application

--- src/main/java/com/victorduarte/servicocartoes/ServicocartoesApplication.java
+++ src/main/java/com/victorduarte/servicocartoes/ServicocartoesApplication.java
@@ -1,11 +1,13 @@
 package com.victorduarte.servicocartoes;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRabbit
 public class ServicocartoesApplication {
 
     public static void main(String[] args) {
